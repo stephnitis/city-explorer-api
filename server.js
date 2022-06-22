edcf7ef6a8828cadffa9cb0ce4d5369067f2bb8e
Stephanie Hill <stephni.hill@gmail.com>
2022-06-22 11:39:11 -0700

    fix

--- server.js
+++ server.js
@@ -15,19 +15,22 @@ console.log(weatherData);
 class Forecast {
   constructor(weatherObject){
     this.datetime = weatherObject.datetime;
-    this.description = weatherObject.description;
+    this.description = weatherObject.weather.description;
   }
 }
 
 app.get('/weatherData', (request, response) => {
-  const searchQuery = request.query;
+  const searchQuery = request.query.searchQuery;
+  // const lat = request.query.lat;
+  // const lon = request.query.lon;
   console.log(searchQuery);
-  let searchResult = weatherData.find(object => object.city_name === searchQuery);
- 
+  let searchResult = weatherData.find(object => object.city_name.toLowerCase() === searchQuery.toLowerCase());
 
-  const result = new Forecast(searchResult);
+  const result = searchResult.data.map(dayObj => new Forecast(dayObj));
+  // const result = new Forecast(searchResult);
   console.log(result);
-})
+  response.status(200).send(result);
+});
 
 // app.get('/hello', (request, response) => {
 //   console.log(request.query.name);
@@ -36,7 +39,7 @@ app.get('/weatherData', (request, response) => {
 // });
 
 app.get('*', (request, response) => {
-  response.send('Does Not Compute');
+  response.status(404).send('Does Not Compute : Not Found');
 });
 
 app.listen(PORT, ()=> console.log(`listening on port ${PORT}`));
