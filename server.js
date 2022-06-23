1f472f99883c304f2b064381c3e111d77e30f349
Stephanie Hill <stephni.hill@gmail.com>
2022-06-22 20:08:01 -0700

    weatherbit

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const axios =require('axios');
 const weatherData = require('./data/weather.json');
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
-console.log(weatherData);
+app.get('/weather', async (req, res, next) => {
+  try {
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let url = `https://api.weatherbit.io/v2.0/normals?lat=${lat}&lon=${lon}&start_day=02-02&end_day=03-01&tp=daily&key=${process.env.WEATHER_API_KEY}`;
+    let results = await axios.get(url);
+    let groomedData = results.data.data.map(day => new Forecast(day));
+    res.status(200).send(groomedData);
+    console.log(results);
+  } catch (error) {
+    next(error)
+  }
+
+});
 
 class Forecast {
   constructor(weatherObject){
@@ -19,27 +33,26 @@ class Forecast {
   }
 }
 
-app.get('/weatherData', (request, response) => {
-  const searchQuery = request.query.searchQuery;
-  // const lat = request.query.lat;
-  // const lon = request.query.lon;
-  console.log(searchQuery);
-  let searchResult = weatherData.find(object => object.city_name.toLowerCase() === searchQuery.toLowerCase());
+// app.get('/weatherData', (request, response) => {
+//   const searchQuery = request.query.searchQuery;
+//   // const lat = request.query.lat;
+//   // const lon = request.query.lon;
+//   console.log(searchQuery);
+//   let searchResult = weatherData.find(object => object.city_name.toLowerCase() === searchQuery.toLowerCase());
 
-  const result = searchResult.data.map(dayObj => new Forecast(dayObj));
-  // const result = new Forecast(searchResult);
-  console.log(result);
-  response.status(200).send(result);
-});
-
-// app.get('/hello', (request, response) => {
-//   console.log(request.query.name);
-//   let name = request.query.name
-//   response.send('hello!');
+//   const result = searchResult.data.map(dayObj => new Forecast(dayObj));
+//   // const result = new Forecast(searchResult);
+//   console.log(result);
+//   response.status(200).send(result);
 // });
 
 app.get('*', (request, response) => {
   response.status(404).send('Does Not Compute : Not Found');
+});
+
+app.use((error, req, res, next) =>{
+  console.log(error.message);
+  res.status(500).send(error.message);
 });
 
 app.listen(PORT, ()=> console.log(`listening on port ${PORT}`));
